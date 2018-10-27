522fd6b08e2d8d1f97404d846bf5f8df4a7c8613
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2018-10-28 00:05:50 +0200

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
 
 const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-	entry: './src/app.js', 
-	// entry: './src/playground/redux-101.js',
-	// entry: './src/playground/redux-expensify.js',
-	// entry: './src/playground/hoc.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	mode: 'development',
-	module: {
-		rules: [{
-			loader: 'babel-loader',
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				'style-loader',
-				'css-loader',
-				'sass-loader'
-			]
-		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true 
-	}
+
+module.exports = (env) => {
+	const isProduction = env === 'production';
+	const CSSExtract = new ExtractTextPlugin('styles.css');
+
+	return {
+		entry: './src/app.js', 
+		// entry: './src/playground/redux-101.js',
+		// entry: './src/playground/redux-expensify.js',
+		// entry: './src/playground/hoc.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		mode: 'development',
+		module: {
+			rules: [{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: CSSExtract.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					]
+				})
+			}]
+		},
+		plugins: [
+			CSSExtract
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true 
+		}
+	};
+
 };
